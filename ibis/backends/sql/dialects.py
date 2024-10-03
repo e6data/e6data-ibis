8f4c7420eece797c3cf64481b0ac7c1e9d61a183
adithyakanamarlapudi <kanamarlapudi@e6x.io>
2024-10-03 15:13:26 +0530

    Added E6data support in the dialects.py
    Signed-off-by: adithyakanamarlapudi <kanamarlapudi@e6x.io>

--- ibis/backends/sql/dialects.py
+++ ibis/backends/sql/dialects.py
@@ -18,7 +18,16 @@ from sqlglot.dialects import (
     SQLite,
     Trino,
 )
-from sqlglot.dialects.dialect import rename_func
+from sqlglot.dialects.dialect import (
+    max_or_greatest,
+    min_or_least,
+    rename_func,
+    unit_to_str,
+    regexp_replace_sql,
+    approx_count_distinct_sql,
+    timestrtotime_sql,
+    datestrtodate_sql
+)
 from sqlglot.helper import find_new_name, seq_get
 
 ClickHouse.Generator.TRANSFORMS |= {
@@ -203,7 +212,7 @@ class Flink(Hive):
             sge.ArraySize: rename_func("cardinality"),
             sge.Length: rename_func("char_length"),
             sge.TryCast: lambda self,
-            e: f"TRY_CAST({e.this.sql(self.dialect)} AS {e.to.sql(self.dialect)})",
+                                e: f"TRY_CAST({e.this.sql(self.dialect)} AS {e.to.sql(self.dialect)})",
             sge.DayOfYear: rename_func("dayofyear"),
             sge.DayOfWeek: rename_func("dayofweek"),
             sge.DayOfMonth: rename_func("dayofmonth"),
@@ -432,17 +441,325 @@ Trino.Generator.TRANSFORMS |= {
     sge.LastValue: rename_func("last_value"),
 }
 
+
 class E6data(MySQL):
     class Tokenizer(MySQL.Tokenizer):
-        IDENTIFIERS = ['"'] 
+        IDENTIFIERS = ['"']
     class Generator(MySQL.Generator):
-        TYPE_MAPPING = {
-            sge.DataType.Type.VARCHAR: "STRING",
-            sge.DataType.Type.CHAR: "STRING",
-            sge.DataType.Type.TEXT: "STRING",
+        def unnest_sql(self, expression: sge.Explode) -> str:
+            # Extract array expressions
+            array_expr = expression.args.get("expressions")
+
+            # Format array expressions to SQL
+            if isinstance(array_expr, list):
+                array_expr_sql = ', '.join(self.sql(arg) for arg in array_expr)
+            else:
+                array_expr_sql = self.sql(array_expr)
+
+            # Process the alias
+            alias = self.sql(expression, "alias")
+
+            # Handle the columns for alias arguments (e.g., t(x))
+            alias_args = expression.args.get("alias")
+            alias_columns = ""
+
+            if alias_args and alias_args.args.get("columns"):
+                # Extract the columns for alias arguments
+                alias_columns_list = [self.sql(col) for col in alias_args.args["columns"]]
+                alias_columns = f"({', '.join(alias_columns_list)})"
+
+            # Construct the alias string
+            alias_sql = f" AS {alias}{alias_columns}" if alias else ""
+
+            # Generate the final UNNEST SQL
+            return f"UNNEST({array_expr_sql}){alias_sql}"
+
+        def extract_sql(self, expression: sge.Extract) -> str:
+            unit = expression.this.name
+            expression_sql = self.sql(expression, "expression")
+            extract_str = f"EXTRACT({unit} FROM {expression_sql})"
+            return extract_str
+
+        def generateseries_sql(self, expression: sge.GenerateSeries) -> str:
+            start = expression.args["start"]
+            end = expression.args["end"]
+            step = expression.args.get("step")
+
+            return self.func("SEQUENCE", start, end, step)
+
+        def _last_day_sql(self, expression: sge.LastDay) -> str:
+            # date_expr = self.sql(expression,"this")
+            date_expr = expression.args.get("this")
+            date_expr = date_expr
+            if isinstance(date_expr, sge.Literal):
+                date_expr = f"CAST({date_expr} AS DATE)"
+            return f"LAST_DAY({date_expr})"
+
+        def regexp_replace_sql(self, expression: sge.RegexpReplace) -> str:
+            bad_args = list(filter(expression.args.get, ("position", "occurrence", "modifiers")))
+            if bad_args:
+                self.unsupported(f"REGEXP_REPLACE does not support the following arg(s): {bad_args}")
+            if not "replacement" in expression.args.keys():
+                replacement = ''
+            else:
+                replacement = expression.args["replacement"]
+
+            return self.func(
+                "REGEXP_REPLACE", expression.this, expression.expression, replacement
+            )
+
+        def _from_unixtime_withunit_sql(self, expression: sge.UnixToTime | sge.UnixToStr) -> str:
+            seconds_str = f"'seconds'"
+            milliseconds_str = f"'milliseconds'"
+            if isinstance(expression, sge.UnixToTime):
+                timestamp = self.sql(expression, "this")
+                # scale = expression.args.get("scale")
+                # # this by default value for seconds is been kept for now
+                # if scale is None:
+                #     scale = 'seconds'
+                scale = expression.args.get("scale",
+                                            sge.Literal.string('seconds'))  # Default to 'seconds' if scale is None
+
+                # Extract scale string, ensure it is lowercase and strip any extraneous quotes
+                scale_str = self.sql(scale).lower().strip('"').strip("'")
+                if scale_str == 'seconds':
+                    return self.func("FROM_UNIXTIME_WITHUNIT", timestamp, seconds_str)
+                elif scale_str == 'milliseconds':
+                    return self.func("FROM_UNIXTIME_WITHUNIT", timestamp, milliseconds_str)
+                else:
+                    raise ValueError(
+                        f"Unsupported unit for FROM_UNIXTIME_WITHUNIT: {scale_str} and we only support 'seconds' and 'milliseconds'")
+            else:
+                timestamp = self.sql(expression, "this")
+                if isinstance(expression.this, sge.Div) and (expression.this.right.this == '1000'):
+                    return self.func("FROM_UNIXTIME_WITHUNIT", timestamp, seconds_str)
+                return self.func("FROM_UNIXTIME_WITHUNIT", timestamp, milliseconds_str)
+
+        def _to_unix_timestamp_sql(self, expression: sge.TimeToUnix | sge.StrToUnix) -> str:
+            timestamp = self.sql(expression, "this")
+            # if not (isinstance(timestamp, sge.Cast) and timestamp.to.is_type(sge.DataType.Type.TIMESTAMP)):
+            # if isinstance(timestamp, (sge.Literal, sge.Column)):
+            #     timestamp = f"CAST({timestamp} AS TIMESTAMP)"
+            return self.func("TO_UNIX_TIMESTAMP", timestamp)
+
+        def tochar_sql(self, expression: sge.ToChar) -> str:
+            date_expr = expression.this
+            if (isinstance(date_expr, sge.Cast) and not (date_expr.to.this.name == 'TIMESTAMP')) or (
+                    not isinstance(date_expr, sge.Cast) and
+                    not sge.DataType.is_type(date_expr, sge.DataType.Type.DATE) or sge.DataType.is_type(date_expr,
+                                                                                                        sge.DataType.Type.TIMESTAMP)):
+                date_expr = f"CAST({date_expr} AS TIMESTAMP)"
+            format_expr = self.format_time(expression)
+            return f"TO_CHAR({date_expr},'{format_expr}')"
+
+        def date_trunc_sql(self, expression: sge.DateTrunc | sge.TimestampTrunc):
+            unit = unit_to_str(expression.unit)
+            date = expression.this
+            return self.func("DATE_TRUNC", unit, date)
+
+        def filter_array_sql(self, expression: sge.ArrayFilter) -> str:
+            cond = expression.expression
+            if isinstance(cond, sge.Lambda) and len(cond.expressions) == 1:
+                alias = cond.expressions[0]
+                cond = cond.this
+            elif isinstance(cond, sge.Predicate):
+                alias = "_u"
+            else:
+                self.unsupported("Unsupported filter condition")
+                return ""
+
+            # Check for aggregate functions
+            if any(isinstance(node, sge.AggFunc) for node in cond.find_all(sge.Expression)):
+                raise ValueError("array filter's Lambda expression are not supported with aggregate functions")
+                return ""
+
+            lambda_expr = f"{alias} -> {self.sql(cond)}"
+            return f"FILTER_ARRAY({self.sql(expression.this)}, {lambda_expr})"
+
+        def bracket_sql(self, expression: sge.Bracket) -> str:
+            return self.func(
+                "ELEMENT_AT",
+                expression.this,
+                seq_get(
+                    apply_index_offset(
+                        expression.this,
+                        expression.expressions,
+                        1 - expression.args.get("offset", 0),
+                    ),
+                    0,
+                ),
+            )
+
+        def format_date_sql(self, expression: sge.TimeToStr) -> str:
+            date_expr = expression.this
+            format_expr = self.format_time(expression)
+            format_expr_quoted = f"'{format_expr}'"
+            if isinstance(date_expr, sge.CurrentDate) or isinstance(date_expr, sge.CurrentTimestamp) or isinstance(
+                    date_expr, sge.TsOrDsToDate):
+                return self.func("FORMAT_DATE", date_expr, format_expr_quoted)
+            if isinstance(date_expr, sge.Cast) and not (
+                    date_expr.to.this.name == 'TIMESTAMP' or date_expr.to.this.name == 'DATE'):
+                date_expr = f"CAST({date_expr} AS DATE)"
+            return self.func("FORMAT_DATE", date_expr, format_expr_quoted)
+
+        def interval_sql(self, expression: sge.Interval) -> str:
+            if expression.this and expression.unit:
+                value = expression.this.name
+                unit = expression.unit.name
+                interval_str = f"INTERVAL {value} {unit}"
+                return interval_str
+            else:
+                return ""
+
+        UNSIGNED_TYPE_MAPPING = {
+            sge.DataType.Type.UBIGINT: "BIGINT",
+            sge.DataType.Type.UINT: "INT",
+            sge.DataType.Type.UMEDIUMINT: "INT",
+            sge.DataType.Type.USMALLINT: "INT",
+            sge.DataType.Type.UTINYINT: "INT",
+            sge.DataType.Type.UDECIMAL: "DECIMAL",
         }
-        
+
+        CAST_SUPPORTED_TYPE_MAPPING = {
+            sge.DataType.Type.NCHAR: "CHAR",
+            sge.DataType.Type.VARCHAR: "VARCHAR",
+            sge.DataType.Type.INT: "INT",
+            sge.DataType.Type.TINYINT: "INT",
+            sge.DataType.Type.SMALLINT: "INT",
+            sge.DataType.Type.MEDIUMINT: "INT",
+            sge.DataType.Type.BIGINT: "BIGINT",
+            sge.DataType.Type.BOOLEAN: "BOOLEAN",
+            sge.DataType.Type.DATE: "DATE",
+            sge.DataType.Type.DATE32: "DATE",
+            sge.DataType.Type.FLOAT: "FLOAT",
+            sge.DataType.Type.DOUBLE: "DOUBLE",
+            sge.DataType.Type.TIMESTAMP: "TIMESTAMP",
+            sge.DataType.Type.TIMESTAMPTZ: "TIMESTAMP",
+            sge.DataType.Type.TIMESTAMPNTZ: "TIMESTAMP",
+            sge.DataType.Type.TEXT: "VARCHAR",
+            sge.DataType.Type.TINYTEXT: "VARCHAR",
+            sge.DataType.Type.MEDIUMTEXT: "VARCHAR",
+            sge.DataType.Type.DECIMAL: "DECIMAL"
+        }
+
+        TYPE_MAPPING = {
+            **UNSIGNED_TYPE_MAPPING,
+            **CAST_SUPPORTED_TYPE_MAPPING,
+            sge.DataType.Type.JSON: "JSON",
+            sge.DataType.Type.STRUCT: "STRUCT",
+            sge.DataType.Type.ARRAY: "ARRAY"
+        }
+
         TRANSFORMS = {
-            sge.Concat: lambda self, e: f"concat({self.sql(e.left)}, {self.sql(e.right)})",
-            sge.Length: rename_func("length"),
+            # sge.Concat: lambda self, e: f"concat({self.sql(e.left)}, {self.sql(e.right)})",
+            **generator.Generator.TRANSFORMS,
+            sge.AnyValue: rename_func("ARBITRARY"),
+            sge.ApproxDistinct: approx_count_distinct_sql,
+            sge.ApproxQuantile: rename_func("APPROX_PERCENTILE"),
+            sge.ArgMax: rename_func("MAX_BY"),
+            sge.ArrayAgg: rename_func("COLLECT_LIST"),
+            sge.ArrayConcat: rename_func("ARRAY_CONCAT"),
+            sge.ArrayContains: rename_func("ARRAY_CONTAINS"),
+            sge.ArrayFilter: filter_array_sql,
+            sge.ArrayToString: rename_func("ARRAY_JOIN"),
+            sge.ArraySize: rename_func("size"),
+            sge.AtTimeZone: lambda self, e: self.func(
+                "DATETIME", e.this, e.args.get("zone")
+            ),
+            sge.BitwiseLeftShift: lambda self, e: self.func("SHIFTLEFT", e.this, e.expression),
+            sge.BitwiseNot: lambda self, e: self.func("BITWISE_NOT", e.this),
+            sge.BitwiseOr: lambda self, e: self.func("BITWISE_OR", e.this, e.expression),
+            sge.BitwiseRightShift: lambda self, e: self.func("SHIFTRIGHT", e.this, e.expression),
+            sge.BitwiseXor: lambda self, e: self.func("BITWISE_XOR", e.this, e.expression),
+            sge.Bracket: bracket_sql,
+            sge.CurrentDate: lambda *_: "CURRENT_DATE",
+            sge.CurrentTimestamp: lambda *_: "CURRENT_TIMESTAMP",
+            sge.Date: lambda self, e: self.func("DATE", e.this),
+            sge.DateAdd: lambda self, e: self.func(
+                "DATE_ADD",
+                unit_to_str(e),
+                _to_int(e.expression),
+                e.this,
+            ),
+            sge.DateDiff: lambda self, e: self.func(
+                "DATE_DIFF",
+                unit_to_str(e),
+                e.expression,
+                e.this,
+            ),
+            sge.DateTrunc: date_trunc_sql,
+            sge.Explode: unnest_sql,
+            sge.Extract: extract_sql,
+            sge.FirstValue: rename_func("FIRST_VALUE"),
+            sge.FromTimeZone: lambda self, e: self.func(
+                "CONVERT_TIMEZONE", "'UTC'", e.args.get("zone"), e.this
+            ),
+            sge.GenerateSeries: generateseries_sql,
+            sge.GroupConcat: lambda self, e: self.func(
+                "LISTAGG" if e.args.get("within_group") else "STRING_AGG",
+                e.this,
+                e.args.get("separator") or sge.Literal.string(',')
+            ),
+            sge.Interval: interval_sql,
+            sge.JSONExtract: lambda self, e: self.func("json_extract", e.this, e.expression),
+            sge.JSONExtractScalar: lambda self, e: self.func("json_extract", e.this, e.expression),
+            sge.Lag: lambda self, e: self.func("LAG", e.this, e.args.get("offset")),
+            sge.LastDay: _last_day_sql,
+            sge.LastValue: rename_func("LAST_VALUE"),
+            sge.Lead: lambda self, e: self.func("LEAD", e.this, e.args.get("offset")),
+            sge.Length: rename_func("LENGTH"),
+            sge.Log: rename_func("LN"),
+            sge.Max: max_or_greatest,
+            sge.MD5Digest: lambda self, e: self.func("MD5", e.this),
+            sge.Min: min_or_least,
+            sge.Mod: lambda self, e: self.func("MOD", e.this, e.expression),
+            sge.Nullif: rename_func("NULLIF"),
+            sge.Pow: rename_func("POWER"),
+            sge.RegexpExtract: rename_func("REGEXP_EXTRACT"),
+            sge.RegexpLike: lambda self, e: self.func("REGEXP_LIKE", e.this, e.expression),
+            sge.RegexpReplace: regexp_replace_sql,
+            sge.RegexpSplit: rename_func("SPLIT_PART"),
+            # sge.Select: select_sql,
+            sge.Split: rename_func("SPLIT"),
+            sge.Stddev: rename_func("STDDEV"),
+            sge.StddevPop: rename_func("STDDEV_POP"),
+            sge.StrPosition: lambda self, e: self.func(
+                "LOCATE", e.args.get("substr"), e.this, e.args.get("position")
+            ),
+            sge.StrToDate: lambda self, e: self.func("TO_DATE", e.this, self.format_time(e)),
+            sge.StrToTime: lambda self, e: self.func("TO_TIMESTAMP", e.this, self.format_time(e)),
+            sge.StrToUnix: _to_unix_timestamp_sql,
+            sge.StartsWith: rename_func("STARTS_WITH"),
+            # sge.Struct: struct_sql,
+            sge.TimeToStr: format_date_sql,
+            sge.TimeStrToTime: timestrtotime_sql,
+            sge.TimeStrToDate: datestrtodate_sql,
+            sge.TimeToUnix: _to_unix_timestamp_sql,
+            sge.Timestamp: lambda self, e: self.func("TIMESTAMP", e.this),
+            sge.TimestampAdd: lambda self, e: self.func(
+                "TIMESTAMP_ADD", unit_to_str(e), e.expression, e.this
+            ),
+            sge.TimestampDiff: lambda self, e: self.func(
+                "TIMESTAMP_DIFF",
+                unit_to_str(e),
+                e.expression,
+                e.this,
+            ),
+            sge.TimestampTrunc: date_trunc_sql,
+            sge.ToChar: tochar_sql,
+            sge.Trim: lambda self, e: self.func("TRIM", e.this, ' '),
+            sge.TsOrDsAdd: lambda self, e: self.func(
+                "DATE_ADD",
+                unit_to_str(e),
+                _to_int(e.expression),
+                e.this,
+            ),
+            sge.TsOrDsDiff: lambda self, e: self.func(
+                "DATE_DIFF",
+                unit_to_str(e),
+                e.expression,
+                e.this,
+            ),
+            sge.UnixToTime: _from_unixtime_withunit_sql,
+            sge.UnixToStr: _from_unixtime_withunit_sql
         }
